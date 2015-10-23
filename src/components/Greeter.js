6d092e3c96f35ba6da9786b0a78658c97cad7e2c
Tuukka Laitinen <tuukka.mj.laitinen@gmail.com>
2015-10-23 13:32:38 +0300

    Geolocation

--- src/components/Greeter.js
+++ src/components/Greeter.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import history from 'history';
-//import { Link } from 'react-router';
+import { Link } from 'react-router';
 
 const Greeter = React.createClass({
     render: function(){
         //console.log(this.props);
         const { name } = this.props.params;
         return (
-            <h1>
-                Hello {name}
-            </h1>
+            <div>
+                <h1>
+                    Hello {name}
+                </h1>
+                <Link to="/">
+                    Back
+                </Link>
+            </div>
         );
     }
 });
--- src/components/SimpleMap.js
+++ src/components/SimpleMap.js
@@ -1,37 +1,93 @@
 import React from 'react';
-import { GoogleMap, Marker } from "react-google-maps";
+import { Link } from 'react-router';
+import { GoogleMap, Marker, InfoWindow, Circle } from 'react-google-maps';
+import canUseDOM from 'can-use-dom';
+import raf from 'raf';
 
-//const SimpleMap = React.createClass({
-//    render: function() {
+const geolocation = (
+  canUseDOM && navigator.geolocation || {
+    getCurrentPosition: (success, failure) => {
+      failure("Your browser doesn't support geolocation.");
+    },
+  }
+);
 
 export default class SimpleMap extends React.Component{
-    /*state = {
-        markers: [{
-            position: {
-                lat: 60.2154805,
-                lng: 24.8174217,
-            },
-            key: "Leppävaara",
-            defaultAnimation: 2
-        }],
-    },*/
-    render(){
+
+    state = {
+        center: null,
+        content: null,
+        radius: 4000,
+        zoom: 12
+    }
+
+    componentDidMount () {
+        geolocation.getCurrentPosition((position) => {
+            this.setState({
+                center: {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                },
+                content: "Location found."
+            });
+
+            const tick = () => {
+                this.setState({ radius: Math.max(this.state.radius - 80, 10) });
+                //this.setState({ zoom: Math.max(this.state.zoom + 1, 0) });
+
+                if (this.state.radius > 100) {
+                    raf(tick);
+                }
+
+                // -------- Karttazoomi-animaatio ----------
+                //if (this.state.zoom < 14){
+                //    raf(tick);
+                //}
+            };
+            raf(tick);
+
+        }, (reason) => {
+            this.setState({
+                center: {
+                    lat: 60.2154805,
+                    lng: 24.8174217
+                },
+                content: `Error: The Geolocation service failed (${ reason }).`
+            });
+        });
+    }
+
+    render () {
+        const {center, content, radius, zoom} = this.state;
+        let contents = [];
+
+        if (center) {
+            contents = contents.concat([
+                (<InfoWindow key="info" position={center} content={content} />),
+                (<Circle key="circle" center={center} radius={radius} options={{
+                        fillColor: "blue",
+                        fillOpacity: 0.3,
+                        strokeColor: "blue",
+                        strokeOpacity: 0.8,
+                        strokeWeight: 0.6,
+                    }}
+                />),
+            ]);
+        }
+
+        console.log(...this.props);
+
         return (
             <GoogleMap containerProps={{
-                    style: {
-                        height: "500px",
-                    },
-                }}
-                defaultZoom={10}
-                defaultCenter={{lat: 60.2154805, lng: 24.8174217}}>
-                <Marker
-                    position={{lat: 60.2154805, lng: 24.8174217}}
-                    defaultAnimation={2}>
-                </Marker>
+                ...this.props,
+                style: {
+                    height: "500px",
+                },
+            }}
+            zoom={zoom}
+            center={center}>
+                {contents}
             </GoogleMap>
-
         );
     }
 }
-
-//export default SimpleMap;
